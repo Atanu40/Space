b51d002be33457dec53255f89232ddfc26c08566
Atanu <mightyapon98@gmail.com>
2023-03-06 17:53:18 +0600

    Final Update

--- main.js
+++ main.js
@@ -1,23 +1,8 @@
 // import './style.scss'
-$('.menu-toggle').click(function() {
+const menu_btn = document.querySelector(".hamburger");
+const mobile_menu = document.querySelector(".mobile-nav");
 
-    /*--------------------------------------------------------------*/
-    /*open-close Navigation menu btn, 500 milliseconds to drop-close*/
-    /*--------------------------------------------------------------*/
-    $('.nav-list').toggleClass('nav-list-open', 500);
-        
-    /*-------------------------------------------------------------*/
-    /* changes hamburger to red X for close button .open CSS       */
-    /*-------------------------------------------------------------*/
-    $(this).toggleClass('open');
-
-  })
-
-//   function myFunction() {
-//     var element = document.getElementById("nl");
-//     element.classList.toggle("active");
-//     if(nl.classList.display === 'none'){
-//       nl.classList.display = 'block';
-//  }
-// }
-
+menu_btn.addEventListener("click", function () {
+  menu_btn.classList.toggle("is-active");
+  mobile_menu.classList.toggle("is-active");
+});
